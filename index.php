552fc22bd91c29808671bc58d13d0718ccbd6f69
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2023-11-28 18:39:57 +0100

    dashboard update

--- index.php
+++ index.php
@@ -3,9 +3,6 @@ session_start();
 include('php/config.php');
 include('php/page-config.php');
 
-if ($_SESSION['role'] != 'admin') {
-  header("Location: sun_sch_attd.php");
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,17 +17,17 @@ if ($_SESSION['role'] != 'admin') {
     <?php include('partials/_navbar.php') ?>
     <!-- partial --><br><br><br>
     <nav class="bg-gradient-primary col-xl-12 col-12 d-flex flex-row p-3 text-light">
-        <div class="col-sm-8 d-flex align-items-center align-items-baseline my-auto">
-          <h6 class="mb-0">Youth Db</h6>
-          <i class="typcn typcn-chevron-right"></i>
-          <p class="mb-0">Home</p>
-          <i class="typcn typcn-chevron-right"></i>
-          <p class="mb-0">Main Dahboard</p>
-        </div>
-        <div class="input-group col-sm-4">
-          <input type="text" class="form-control form-control-sm" placeholder="Search..." aria-label="search"
-            aria-describedby="search">
-        </div>
+      <div class="col-sm-8 d-flex align-items-center align-items-baseline my-auto">
+        <h6 class="mb-0">Youth Db</h6>
+        <i class="typcn typcn-chevron-right"></i>
+        <p class="mb-0">Home</p>
+        <i class="typcn typcn-chevron-right"></i>
+        <p class="mb-0">Main Dahboard</p>
+      </div>
+      <div class="input-group col-sm-4">
+        <input type="text" class="form-control form-control-sm" placeholder="Search..." aria-label="search"
+          aria-describedby="search">
+      </div>
     </nav>
     <div class="container-fluid page-body-wrapper">
       <!-- partial:partials/_settings-panel.php -->
@@ -41,47 +38,107 @@ if ($_SESSION['role'] != 'admin') {
       <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper">
-          
+
           <div class="row">
             <div class="col-8 m-auto stretch-card">
-              <div class="card">
-                <div class="card-body border-bottom">
-                  <div class="d-flex justify-content-between align-items-center flex-wrap">
-                    <h1 class="py-5 px-2 text-uppercase font-weight-bold text-center">Dashboard is under contruction. <br><br> stay tuned!</h1>
+              <!-- Total Number of Members -->
+              <div class="col-md-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Total Members</h5>
+                    <p class="card-text">1500</p>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Total Number of Visitors -->
+              <div class="col-md-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Total Visitors</h5>
+                    <p class="card-text">500</p>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Total Number of Branches -->
+              <div class="col-md-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Total Branches</h5>
+                    <p class="card-text">10</p>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Total Number of Workers -->
+              <div class="col-md-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Total Workers</h5>
+                    <p class="card-text">200</p>
                   </div>
                 </div>
               </div>
             </div>
+
+            <!-- Table for Upcoming Birthdays -->
+            <div class="row mt-4">
+              <div class="col-md-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Upcoming Birthdays</h5>
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Date</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>John Doe</td>
+                          <td>2023-05-15</td>
+                        </tr>
+                        <tr>
+                          <td>Jane Smith</td>
+                          <td>2023-06-10</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
-
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.php -->
+          <?php include('partials/_footer.php') ?>
+          <!-- partial -->
         </div>
-        <!-- content-wrapper ends -->
-        <!-- partial:partials/_footer.php -->
-        <?php include('partials/_footer.php') ?>
-        <!-- partial -->
+        <!-- main-panel ends -->
       </div>
-      <!-- main-panel ends -->
+      <!-- page-body-wrapper ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-  </div>
-  <!-- container-scroller -->
+    <!-- container-scroller -->
 
-  <!-- base:js -->
-  <script src="vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-  <script src="js/off-canvas.js"></script>
-  <script src="js/hoverable-collapse.js"></script>
-  <script src="js/template.js"></script>
-  <script src="js/settings.js"></script>
-  <script src="js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  
-  <!-- End custom js </body> for this page-->
+    <!-- base:js -->
+    <script src="vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="js/off-canvas.js"></script>
+    <script src="js/hoverable-collapse.js"></script>
+    <script src="js/template.js"></script>
+    <script src="js/settings.js"></script>
+    <script src="js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+
+    <!-- End custom js </body> for this page-->
 </body>
 
 </html>
