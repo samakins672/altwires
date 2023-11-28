dcc71f667ee187a924aeb4c4ca1e2e1d7e33bbd8
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2023-11-28 09:27:08 +0100

    Change dp details

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "afm_attendance");
+$conn = mysqli_connect("localhost", "sannexng_admin", "Samkins.web1", "sannexng_altwires");
 
 if (!$conn) {
   die("Error: Failed to connect to database!");
