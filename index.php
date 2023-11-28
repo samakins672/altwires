985b503dafe5b88709ee1920eea15bef41be42bf
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2023-11-28 18:59:04 +0100

    added stretch card

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ include('php/page-config.php');
         <div class="content-wrapper">
 
           <div class="row">
-            <div class="col-12 m-auto">
+            <div class="col-12 m-auto stretch-card">
               <!-- Total Number of Members -->
               <div class="col-6 col-md-3">
                 <div class="card">
